11c65cd4c317df1d78cfc320fa57a5699c632f80
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-01 22:29:13 +0530

    update: popup

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Popup = () => {
   return (
-    <div className="App">
-      <h2>Ad-Blocker that works</h2>
+    <div className="bg-blue-50 h-full py-8 px-4">
+      <h2 className="text-center text-xl">
+        Chrome extension to add more features to your browser
+      </h2>
     </div>
   );
 };
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -6,15 +6,11 @@ import { createRoot } from "react-dom/client";
 import Popup from "./Popup";
 import { attachTwindStyle } from "@src/shared/style/twind";
 
-function init() {
-  const appContainer = document.querySelector("#app-container");
-  if (!appContainer) {
-    throw new Error("Can not find #app-container");
-  }
-  attachTwindStyle(appContainer, document);
-  const root = createRoot(appContainer);
-  root.render(<Popup />);
+const appContainer = document.querySelector("#app");
+if (!appContainer) {
+  throw new Error("Can not find #app-container");
 }
 
-init();
-
+attachTwindStyle(appContainer, document);
+const root = createRoot(appContainer);
+root.render(<Popup />);

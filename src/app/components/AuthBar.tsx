ed850d0feafae2dc35bd68929781585cc9dcc2b4
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-03 02:13:25 +0530

    update: auth.tsx bar

--- src/app/components/AuthBar.tsx
+++ src/app/components/AuthBar.tsx
@@ -1,20 +1,13 @@
-import { useIsOpenStore } from "@app/stores/useIsOpenStore";
-import { useLoginStore } from "@app/stores/useLoginStore";
 import { ContentBar } from "@app/components/ContentBar";
 import { LoginBar } from "@app/components/LoginBar";
+import { useConfigStore } from "../stores/useConfigStore";
 
 export function AuthBar() {
-  const { open } = useIsOpenStore();
-  const { loggedIn } = useLoginStore();
+  const user = useConfigStore((state) => state.user);
 
   return (
-    <div
-      className="px-2 flex-grow bg-black text-white"
-      style={{
-        display: open ? "block" : "none",
-      }}
-    >
-      {loggedIn ? <ContentBar /> : <LoginBar />}
+    <div className="px-2 flex-grow bg-black text-white">
+      {user ? <ContentBar /> : <LoginBar />}
     </div>
   );
 }

211d6be59f97490682d2b722d7b9b4473fa8d61a
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-03 03:50:44 +0530

    update: login bar

--- src/app/components/LoginBar.tsx
+++ src/app/components/LoginBar.tsx
@@ -1,34 +1,59 @@
-import { useLoginStore } from "@app/stores/useLoginStore";
 import { BlockButton } from "@app/ui/Buttons";
-import React from "react";
+import React, { useState } from "react";
+import { useConfigStore } from "../stores/useConfigStore";
+import { useNotificationStore } from "../stores/useNotificationsStore";
 
 interface LoginBarProps extends React.PropsWithChildren {}
 
 export const LoginBar: React.FC<LoginBarProps> = () => {
-  const { toggleLoggedIn } = useLoginStore();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const { update } = useConfigStore();
+  const { addNotification } = useNotificationStore();
+
   return (
     <form
       className="flex space-x-3"
       onSubmit={async (e) => {
         e.preventDefault();
+        setLoading(true);
+        setError(false);
+
         const form = e.currentTarget;
         const { value: u } = form.elements.namedItem("u") as HTMLInputElement;
         const { value: p } = form.elements.namedItem("p") as HTMLInputElement;
 
-        toggleLoggedIn();
+        try {
+          const res = await chrome.runtime.sendMessage({ type: "login", u, p });
+
+          if (res?.user) update("user", res.user);
+          else throw new Error(res?.message || "Login error");
+        } catch (e) {
+          addNotification({ content: JSON.stringify(e), type: "error" });
+          setError(true);
+        } finally {
+          setLoading(false);
+        }
       }}
     >
       <input
+        required
         className="px-2 h-6 bg-zinc-900 outline-none"
         name="u"
         placeholder="u"
       />
       <input
+        required
         className="px-2 h-6 bg-zinc-900 outline-none"
         name="p"
         placeholder="p"
       />
-      <BlockButton>Ding</BlockButton>
+      <BlockButton
+        loading={loading}
+        className={[error ? "!text-red-700" : "", "w-14 h-6"].join(" ")}
+      >
+        Dick
+      </BlockButton>
     </form>
   );
 };

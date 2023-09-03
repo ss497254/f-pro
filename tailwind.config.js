fdfa30abbc479f9e211b9955dbaeecf9fbc78261
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-03 19:45:37 +0530

    update: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
+import config from "./twind.config";
+
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{tsx,ts}"],
+  theme: config.theme,
 };

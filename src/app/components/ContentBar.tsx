422c5a6e94c972d7865424cf904a1aafdabd8523
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-05 03:11:43 +0530

    update: show content based on permission

--- src/app/components/ContentBar.tsx
+++ src/app/components/ContentBar.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { SettingsDropdown } from "@app/components/Dropdowns/SettingsDropdown";
-import { ChatsDropdown } from "@app/components/Dropdowns/ChatsDropdown";
-import { ChatgptDropdown } from "@app/components/Dropdowns/ChatgptDropdown";
+import { ZonicDropdown } from "@root/src/app/components/Dropdowns/ZonicDropdown";
+import { MusicDropdown } from "@root/src/app/components/Dropdowns/MusicDropdown";
 import { NotificationsDropdown } from "@app/components/Dropdowns/NotificationDropdown";
+import { useConfigStore } from "@app/stores/useConfigStore";
 
 interface ContentBarProps extends React.PropsWithChildren {}
 
+const PemissionToComponent: Record<string, React.FC<any>> = {
+  zonic: ZonicDropdown,
+  music: MusicDropdown,
+};
+
 export const ContentBar: React.FC<ContentBarProps> = () => {
+  const user = useConfigStore((state) => state.user)!;
+
   return (
     <div className="flex space-x-0.5">
-      <ChatsDropdown />
-      <ChatgptDropdown />
-      <div className="flex-grow" />
+      {user.permissions.map((permission, idx) => {
+        const Component = PemissionToComponent[permission];
+        return <Component key={idx} />;
+      })}
       <NotificationsDropdown />
       <SettingsDropdown />
     </div>

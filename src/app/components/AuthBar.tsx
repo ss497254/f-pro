2ad5f9b47b7872c6628388ba61664b3c289ab3f8
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-06 00:42:37 +0530

    feat: add error boundary in authbar

--- src/app/components/AuthBar.tsx
+++ src/app/components/AuthBar.tsx
@@ -1,14 +1,30 @@
 import { ContentBar } from "@app/components/ContentBar";
 import { LoginBar } from "@app/components/LoginBar";
-import { useConfigStore } from "../stores/useConfigStore";
-import { BarNotification } from "../ui/Notifications/BarNotification";
+import { useConfigStore } from "@app/stores/useConfigStore";
+import { BarNotification } from "@app/ui/Notifications/BarNotification";
+import { useNotificationStore } from "@app/stores/useNotificationsStore";
+import { ErrorBoundary } from "@app/ui/ErrorBoundary";
 
 export function AuthBar() {
   const user = useConfigStore((state) => state.user);
+  const addNotification = useNotificationStore(
+    (state) => state.addNotification
+  );
 
   return (
-    <div className="px-2 flex-grow flex justify-between bg-black text-white">
-      {user ? <ContentBar /> : <LoginBar />}
+    <div className="px-2 flex-grow flex justify-between items-center">
+      <ErrorBoundary
+        onError={(e, eInfo) =>
+          addNotification({
+            type: "error",
+            content: e.message,
+            extra: { eInfo, ...e },
+          })
+        }
+        fallback={<div className="text-red-500">Something went wrong.</div>}
+      >
+        {user ? <ContentBar /> : <LoginBar />}
+      </ErrorBoundary>
       <BarNotification />
     </div>
   );
---
+++ src/app/ui/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  fallback: ReactNode;
+  onError?: (error: Error, errorInfo: ErrorInfo) => void;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.props.onError?.(error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}

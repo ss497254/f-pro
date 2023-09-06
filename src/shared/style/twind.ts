0052e7b29faabb2766bb9ecc2907ac334bb4aa8e
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-07 00:37:06 +0530

    update: add custom styles is twind

--- src/shared/style/twind.ts
+++ src/shared/style/twind.ts
@@ -2,11 +2,41 @@ import { twind, cssom, observe } from "@twind/core";
 import "construct-style-sheets-polyfill";
 import config from "@root/twind.config";
 
+const customStyles = `
+::-webkit-scrollbar {
+  width: 7.5px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: var(--surface2);
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: var(--surface3);
+  border-radius: 10px;
+}
+
+.scroll-thin::-webkit-scrollbar {
+  width: 5px;
+}
+
+.hide-scroll::-webkit-scrollbar-thumb {
+  background-color: transparent;
+}
+
+.hide-scroll:hover::-webkit-scrollbar-thumb {
+  background-color: var(--surface3);
+}`;
+
 export function attachTwindStyle<T extends { adoptedStyleSheets: any }>(
   observedElement: Element,
   documentOrShadowRoot: T
 ) {
   const sheet = cssom(new CSSStyleSheet());
+  const styles = customStyles.split("\n\n");
+
+  for (const style of styles) sheet.target.insertRule(style);
+
   const tw = twind(config, sheet);
   observe(tw, observedElement);
   documentOrShadowRoot.adoptedStyleSheets = [sheet.target];

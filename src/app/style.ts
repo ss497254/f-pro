e588456905c2dc0375d9eebc4fc22da508d7a25e
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-07 00:40:59 +0530

    fix: root element position

--- src/app/style.ts
+++ src/app/style.ts
@@ -3,7 +3,7 @@ import { setDarkMode } from "./lib/change-theme";
 export const addStyles = (element: HTMLDivElement) => {
   setDarkMode();
 
-  element.style.position = "absolute";
+  element.style.position = "fixed";
   element.style.zIndex = "2147483647";
   element.style.top = "0";
   element.style.left = "0";

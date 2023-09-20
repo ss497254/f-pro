10e0fe5ba3a986b5e38aeba89647936a1f2e76fa
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-21 03:30:19 +0530

    update: login handler in background script

--- src/background/handler/login.ts
+++ src/background/handler/login.ts
@@ -1,20 +1,41 @@
+import { setConfig } from "../config";
+import { initializeWSClient } from "../lib/ws-client-store";
 import { Cfetch } from "../utils/fetch";
+import { getPortName } from "../utils/port";
 
 interface LoginResponse {
   token: string;
   user: any;
 }
 
-export const handleLogin = async (data: {
-  username: string;
-  password: string;
-}) => {
-  const res = await Cfetch<LoginResponse>("/user/login", {
-    method: "POST",
-    body: JSON.stringify(data),
-  });
+export const handleLogin = async (
+  username: string,
+  password: string,
+  sendResponse: (response?: any) => void
+) => {
+  try {
+    const res = await Cfetch<LoginResponse>("/user/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+    });
 
-  if (res.success) return res.data;
+    if (res.success) {
+      const { user, token } = res.data;
+      const port = getPortName();
 
-  throw new Error(res.message);
+      setConfig("token", token);
+      initializeWSClient(port);
+
+      return sendResponse({
+        type: "success",
+        user,
+        port,
+      });
+    }
+
+    throw new Error(res.message);
+  } catch (e: any) {
+    console.warn("Login error:", e);
+    sendResponse({ type: "error", message: e.message });
+  }
 };

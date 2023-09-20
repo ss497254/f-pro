06f2f53b9e389dc79dfb82643d8dc1672409f1be
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-21 03:27:53 +0530

    update: ColorModeBtn style

--- src/app/components/ChangeColorModeBtn.tsx
+++ src/app/components/ChangeColorModeBtn.tsx
@@ -1,13 +1,13 @@
-import { DarkModeIcon } from "@app/icons";
-import { changeColorMode } from "@app/lib/change-theme";
+import { DarkModeIcon } from "app/icons";
+import { changeColorMode } from "app/lib/change-theme";
 
 export const ChangeColorModeBtn = () => {
   return (
     <button
-      className="p-1.5 hover:bg-surface2 transition duration-150"
+      className="p-[7px] hover:bg-surface2 transition duration-150"
       onClick={changeColorMode}
     >
-      <DarkModeIcon size={16.5} />
+      <DarkModeIcon size={16} />
     </button>
   );
 };

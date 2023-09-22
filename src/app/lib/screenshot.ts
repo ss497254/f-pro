3aa7335cb319d773ac09e18588a41362c3a064a3
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-22 21:04:36 +0530

    feat: add screenshot fn in content-script

---
+++ src/app/lib/screenshot.ts
@@ -0,0 +1,27 @@
+import { getChannelStore } from "app/stores/getChannelStore";
+import { toggleVisibility } from "./toggle-visibility";
+
+let lastScreenshot = "";
+export let clearLastScreenShot = () => {};
+
+export const takeScreenShot = async (channel: string) => {
+  toggleVisibility();
+
+  await new Promise((res) => setTimeout(res, 500));
+
+  const data = await chrome.runtime.sendMessage({ type: "capture" });
+  if (data.type === "capture" && data.url) {
+    clearLastScreenShot();
+    lastScreenshot = data.url;
+
+    getChannelStore(channel).setState({ hasAttachment: true });
+    clearLastScreenShot = () => {
+      lastScreenshot = "";
+      getChannelStore(channel).setState({ hasAttachment: false });
+    };
+  }
+
+  toggleVisibility();
+};
+
+export const getLastScreenShot = () => lastScreenshot;

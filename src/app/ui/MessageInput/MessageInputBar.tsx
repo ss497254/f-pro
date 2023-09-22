e33eeb0bae58ec61876e4b92c09c089ccccfb9db
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-22 21:30:24 +0530

    update: attachment preview in message input

--- src/app/ui/MessageInput/MessageInputBar.tsx
+++ src/app/ui/MessageInput/MessageInputBar.tsx
@@ -1,8 +1,9 @@
-import { SendIcon } from "@app/icons";
-import { IconButton } from "@app/ui/Buttons/IconButton";
-import { getMessageStore } from "@root/src/app/lib/getMessageStore";
+import { CloseIcon, SendIcon } from "app/icons";
+import { clearLastScreenShot, getLastScreenShot } from "app/lib/screenshot";
+import { getChannelStore } from "app/stores/getChannelStore";
+import { IconButton } from "app/ui/Buttons/IconButton";
 import React, { useEffect, useRef } from "react";
-import { Spinner } from "../Spinner";
+import { Button } from "../Buttons";
 import { ExpandingTextArea } from "./ExpandingTextArea";
 
 interface Props {
@@ -10,17 +11,25 @@ interface Props {
 }
 
 export const MessageInputBar: React.FC<Props> = ({ channel }) => {
-  const [addMessage, isSubmitting] = getMessageStore(channel)((state) => [
-    state.addMessage,
-    state.isSubmitting,
-  ]);
+  const store = getChannelStore(channel);
+  const sendMessage = store((state) => state.sendMessage);
+  const isSubmitting = store((state) => state.isSubmitting);
+  const hasAttachment = store((state) => state.hasAttachment);
 
   const ref = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const isRunning = useRef(false);
 
-  const onSubmit = () => {
-    if (isSubmitting) return;
+  const onSubmit = async () => {
+    if (isRunning.current || isSubmitting) return;
 
-    addMessage(ref.current?.innerText || "(empty)");
+    isRunning.current = true;
+    await sendMessage(
+      ref.current?.innerText || "(empty)",
+      hasAttachment ? "#" : undefined
+    );
+    if (hasAttachment) clearLastScreenShot();
+    isRunning.current = false;
 
     if (ref.current) {
       ref.current.innerText = "";
@@ -41,16 +50,40 @@ export const MessageInputBar: React.FC<Props> = ({ channel }) => {
   }, []);
 
   return (
-    <div className="flex items-end p-2 border-t border-surface2">
-      <ExpandingTextArea ref={ref} />
-      <IconButton
-        size={40}
-        onClick={onSubmit}
-        disabled={isSubmitting}
-        className="!p-2 ml-3 hover:rounded-full duration-200 h-fit"
-      >
-        {isSubmitting ? <Spinner /> : <SendIcon size={22} />}
-      </IconButton>
-    </div>
+    <>
+      {hasAttachment && (
+        <div className="bg-surface3">
+          <img
+            key={clearLastScreenShot.toString()}
+            ref={imageRef}
+            className="border border-surface4 max-h-96 mx-auto"
+          ></img>
+          <div className="py-1 px-2 flex justify-between items-center">
+            <Button
+              className="hover:!bg-surface1"
+              onClick={() => {
+                imageRef.current!.src = getLastScreenShot();
+              }}
+            >
+              View attachment
+            </Button>
+            <IconButton onClick={clearLastScreenShot}>
+              <CloseIcon size={15} />
+            </IconButton>
+          </div>
+        </div>
+      )}
+      <div className="flex items-end p-2 border-t border-surface2">
+        <ExpandingTextArea ref={ref} />
+        <IconButton
+          size={40}
+          onClick={isSubmitting ? undefined : onSubmit}
+          loading={isSubmitting}
+          className="!p-2 ml-3 duration-200"
+        >
+          <SendIcon size={24} />
+        </IconButton>
+      </div>
+    </>
   );
 };

a9a9223c172c537f8bb1b543198f53437e7762d4
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-22 21:35:59 +0530

    update: auto scroll on new message scroll-to-bottom hook

--- src/app/hooks/useScrollToBottom.ts
+++ src/app/hooks/useScrollToBottom.ts
@@ -1,14 +1,35 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
+import { getChannelStore } from "app/stores/getChannelStore";
 
-export const useScrollToBottom = () => {
-  const ref = useRef<any>();
+export function useScrollToBottom(channel?: string) {
+  // for auto-scroll
+  let newMessages;
+  if (channel)
+    newMessages = getChannelStore(channel)((state) => state.newMessages);
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [autoScroll, setAutoScroll] = useState(true);
+
+  function scrollDomToBottom() {
+    const dom = scrollRef.current;
+    if (dom) {
+      requestAnimationFrame(() => {
+        setAutoScroll(true);
+        dom.scrollTo(0, dom.scrollHeight);
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (autoScroll) {
+      scrollDomToBottom();
+    }
+  }, [newMessages, autoScroll]);
 
   return {
-    ref,
-    scroll: () => {
-      if (ref.current) {
-        ref.current.scrollTop = ref.current.scrollHeight;
-      }
-    },
+    scrollRef,
+    autoScroll,
+    setAutoScroll,
+    scrollDomToBottom,
   };
-};
+}

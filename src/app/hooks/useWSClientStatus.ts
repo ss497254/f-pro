4e6b677c9625f735cba8d628ee3650c19b31ea2c
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-27 23:03:20 +0530

    feat: add useWSClientStatus hook

---
+++ src/app/hooks/useWSClientStatus.ts
@@ -0,0 +1,16 @@
+import { getPort } from "app/lib/port";
+import { useEffect, useState } from "react";
+
+export const useWSClientStatus = () => {
+  const [status, setStatus] = useState("");
+  useEffect(() => {
+    const port = getPort();
+    const listner = (x: any) => {
+      if (x.event === "state") setStatus(x.data.status);
+    };
+
+    port.onMessage.addListener(listner);
+    return () => port.onMessage.removeListener(listner);
+  }, []);
+  return status;
+};

c5c4a7f8cddadf840287cae29dfaecf634fecffc
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-10-01 06:28:49 +0530

    update: add LoginResponse type in ResponseType

--- src/types/ResponseType.ts
+++ src/types/ResponseType.ts
@@ -1,5 +1,13 @@
+import { IChannel, IUser } from ".";
+
 export interface ResponseType<T> {
   success: boolean;
   data: T;
   message?: string;
 }
+
+export type LoginResponse = {
+  token: string;
+  user: IUser;
+  channels: IChannel[];
+};

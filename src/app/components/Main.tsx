149dfe361764fe0567b17cc04cc1468c7a853dff
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-10-27 00:55:50 +0530

    update: add opacity in Main.tsx

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,14 +1,16 @@
 import { AuthBar } from "app/components/AuthBar";
 import { CloseIcon } from "app/icons";
+import { useConfigStore } from "app/stores/useConfigStore";
 import { useIsOpenStore } from "app/stores/useIsOpenStore";
 
 export default function BottomBar() {
   const { open, toggleOpen } = useIsOpenStore();
+  const opacity = useConfigStore((s) => s.opacity);
 
   return (
     <div
-      className="flex h-6 bg-surface1 text-base text-color1"
-      style={{ width: open ? "100%" : "fit-content" }}
+      className="flex h-6 bg-opacity-0 text-base text-color1"
+      style={{ opacity, width: open ? "100%" : "fit-content" }}
     >
       {open && <AuthBar />}
       <button className="h-6 w-6 p-1 hover:bg-surface2" onClick={toggleOpen}>
